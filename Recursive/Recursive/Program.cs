69a9f5008e6d30b9ee0192cef2c23d37b3526db0
ShreyasHingarh <shreyas.hingarh2@gmail.com>
2022-02-27 11:02:12 -0800

    yes

---
+++ Recursive/Recursive/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Recursive
+{
+    class Program
+    {
+       
+        public static void CountDown(int startNumber)
+        {
+            Console.WriteLine(startNumber);
+            if (startNumber == 0) return;
+            startNumber--;
+            CountDown(startNumber);
+        }
+        static void Main(string[] args)
+        {
+            int numero = 30;
+            CountDown(numero);
+        }
+    }
+}
